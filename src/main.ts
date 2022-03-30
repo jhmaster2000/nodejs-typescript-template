6b9ededae50dbbf02aff8ce57bd95e80786ef0e2
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2022-03-30 04:04:19 -0300

    all working now

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,1 @@
 console.log('Hello World!');
-
-try {
-    throw new Error('testing source mapping');
-} catch (e) {
-    if ((e as Error).stack?.includes('.ts:')) {
-        console.info('Stack trace mapped back to TypeScript should indicate correct error line number');
-        console.info(e);
-    }
-}
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,13 +1,3 @@
-// Import without specifying the extension. `./Lib.js` would look odd in a .ts file
-test('constructor', () => {
+test('sample test', () => {    
     expect(true).toBeTruthy();
 });
-
-try {
-    throw new Error('testing source mapping in Jest');
-} catch (e) {
-    if ((e as Error).stack?.includes('.ts:')) {
-        console.info('Stack trace mapped back to TypeScript should indicate correct error line number');
-        console.info(e);
-    }
-}
