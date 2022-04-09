7ec1662e5457afefa933d972b23f7b8845266aa2
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2022-04-09 09:46:22 -0300

    multiloader

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import ENV, { getHostname, getPlatform } from './subfile';
+import ENV, { getHostname, getPlatform } from './subfile.js';
+import test from './test.yml';
 
 console.log(`Hello ${getPlatform()}!`);
 console.log(`Hostname: ${getHostname()}`);
+console.log(test);
+console.log(test.prop);
 
 export default function envVarExists(envVar: string): boolean {
     if (ENV[envVar]) return true;
