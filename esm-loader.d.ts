65ea02828f9b73c743d5734988c9157b9f8b4b0c
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2022-04-09 06:33:18 -0300

    custom exports are allowed on loaders

--- esm-loader.d.ts
+++ esm-loader.d.ts
@@ -5,6 +5,7 @@ declare global {
         resolve?: resolve;
         load?: load;
         globalPreload?: globalPreload;
+        [key: string]: any;
     }
 
     type TypedArray = Int8Array | Uint8Array | Uint8ClampedArray | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array | BigInt64Array | BigUint64Array;
